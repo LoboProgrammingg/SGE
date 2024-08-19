f347bf7a8521343fdcd7bb7ae8531ec78acffaf6
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-19 14:40:11 -0400

    Criando uma aba de pesquisa para brands

--- brands/views.py
+++ brands/views.py
@@ -6,3 +6,12 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
