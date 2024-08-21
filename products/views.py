e482d49bcc75f4cd0c4aad9017d341f29dfafecd
loboprogrammingg <matheusloboo2001@gmail.com>
2024-08-21 17:11:31 -0400

    Adicionando filtros nas pesquisas de produtos

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
+from categories.models import Category
+from brands.models import Brand
 
 
 class ProductListView(ListView):
@@ -12,11 +14,29 @@ class ProductListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         title = self.request.GET.get('title')
+        serie_number = self.request.GET.get('serie_number')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
 
         if title:
             queryset = queryset.filter(title__icontains=title)
 
+        if serie_number:
+            queryset = queryset.filter(serie_number__icontains=serie_number)
+        
+        if category:
+            queryset = queryset.filter(category__id=category)
+
+        if brand:
+            queryset = queryset.filter(brand__id=brand)
+
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
+        return context
 
 
 class ProductCreateView(CreateView):
