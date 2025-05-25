87897c878fe4d97d46c5d1fef80b51e7baf8618f
loboprogrammingg <matheusloboo2001@gmail.com>
2025-05-25 12:54:28 -0400

    Ajustando Dockerfile

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ class ProductListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = models.Product
     template_name = 'product_list.html'
     context_object_name = 'products'
-    paginate_by = 10
+    paginate_by = 5
     permission_required = 'products.view_product'
 
     def get_queryset(self):
